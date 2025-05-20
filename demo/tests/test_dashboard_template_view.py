377812714b776c8e64a29c8b5ea9af5c9ba18780
Ian Waldron <ian.waldron@scribawaldron.com>
2025-05-20 09:37:44 -0700

    update view to use auth mixin

--- demo/tests/test_dashboard_template_view.py
+++ demo/tests/test_dashboard_template_view.py
@@ -2,14 +2,20 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+
 from utils.test.view_test_mixins import TemplateViewTestMixin
+from utils.test.auth_test_mixins import StaffUserTestMixin
 from ..views import DashboardTemplateView
 
 
 User = get_user_model()
 
 
-class TestDashboardTemplateView(TemplateViewTestMixin, TestCase):
+class TestDashboardTemplateView(
+    TemplateViewTestMixin,
+    StaffUserTestMixin,
+    TestCase
+):
 
     @classmethod
     def setUpTestData(cls):
@@ -21,10 +27,3 @@ class TestDashboardTemplateView(TemplateViewTestMixin, TestCase):
         # we need a user that has staff permissions for the client get
         #   request to succeed
         cls.user = User.objects.create(username='example', is_staff=True)
-
-    def test_permission_denied(self):
-        # make sure usernames don't collide
-        user = User.objects.create(username="different-user")
-        self.client.force_login(user)
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 403)
